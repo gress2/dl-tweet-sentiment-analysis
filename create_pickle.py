5ef06c16b529eb72862103f9230a62d21d130c66
Collin Gress <gress126@gmail.com>
2017-12-07 17:52:03 -0600

    add small script to create pickled panda dataframe

---
+++ create_pickle.py
@@ -0,0 +1,17 @@
+import csv
+from sys import argv
+from pandas import read_csv
+
+if len(argv) is not 3:
+    print('Usage: python create_pickle.py <CSV_FILE> <PICKLE_FILE>')
+    exit()
+
+csv_file = argv[1] 
+pickle_file = argv[2]
+
+df = read_csv(csv_file, delimiter=',', quotechar='"', error_bad_lines=False, 
+        doublequote=True, quoting=csv.QUOTE_ALL, skipinitialspace=True) 
+df = df.drop(['SentimentSource'], axis=1)
+df.to_pickle(pickle_file)
+
+
